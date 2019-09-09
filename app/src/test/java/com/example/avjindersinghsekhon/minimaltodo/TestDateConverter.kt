b5210344d529c9d5b42d6e0b05bb6cb648dd5832
Akram Shokri <ashokri.ac@gmail.com>
2019-09-09 23:13:09 +0430

    feat: add unit test for date converter

--- app/src/test/java/com/example/avjindersinghsekhon/minimaltodo/TestDateConverter.kt
+++ app/src/test/java/com/example/avjindersinghsekhon/minimaltodo/TestDateConverter.kt
@@ -1,21 +1,15 @@
 package com.example.avjindersinghsekhon.minimaltodo
 
-import androidx.test.platform.app.InstrumentationRegistry.getInstrumentation
 import com.example.avjindersinghsekhon.minimaltodo.model.convertToLocalDateTime
-import com.example.avjindersinghsekhon.minimaltodo.network.AuthenticationRepository
-import com.example.avjindersinghsekhon.minimaltodo.network.ITokenCallBack
 import org.junit.Test
-import org.junit.runner.RunWith
-import org.junit.runners.JUnit4
 import java.text.SimpleDateFormat
-import java.util.*
 
 /**
  * Created on 2019-09-05, 14:21.
  * @author Akram Shokri
  */
 
-@RunWith(JUnit4::class)
+
 class TestDateConverter {
 
     @Test
