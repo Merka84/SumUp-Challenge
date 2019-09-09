bf78ebf88a440b741036ce6b7d880813d8ade2f5
Akram Shokri <ashokri.ac@gmail.com>
2019-09-09 23:07:18 +0430

    refactor: update get receipt test

--- app/src/androidTest/java/com/example/avjindersinghsekhon/minimaltodo/TestAPICallRepository.kt
+++ app/src/androidTest/java/com/example/avjindersinghsekhon/minimaltodo/TestAPICallRepository.kt
@@ -1,11 +1,15 @@
 package com.example.avjindersinghsekhon.minimaltodo
 
 import androidx.test.filters.SmallTest
+import com.example.avjindersinghsekhon.minimaltodo.model.SumUpReceiptResponse
 import com.example.avjindersinghsekhon.minimaltodo.network.APICallRepository
 import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.junit.runners.JUnit4
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 /**
  * Created on 2019-09-05, 14:21.
@@ -24,6 +28,15 @@ class TestAPICallRepository {
 
     @Test
     fun testReceipt() {
-       repository.getRreceipt("TC263PYMYA")
+       repository.getReceipt("TC263PYMYA", object: Callback<SumUpReceiptResponse> {
+           override fun onFailure(call: Call<SumUpReceiptResponse>, t: Throwable) {
+               assert(t.localizedMessage != null)
+           }
+
+           override fun onResponse(call: Call<SumUpReceiptResponse>, response: Response<SumUpReceiptResponse>) {
+               assert(response.body() != null)
+           }
+
+       })
     }
 }
